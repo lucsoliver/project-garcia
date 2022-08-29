fd7b686b77d014ec7e1415aa6e35b80e5416fcd7
Lucas Oliveira <lucasop888@yahoo.com>
2022-08-29 20:46:57 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <button> Usuário </button>
+        <button> Maps </button>
+        <button> Quiz </button>
+        <button> Vídeo </button>
+      </div>
+    );
+  }
+}
